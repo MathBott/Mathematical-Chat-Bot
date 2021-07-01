4e60f622546430ca456a3660a167b0c2d1b1fa49
Deloris Thompson <deloristhompson28@gmail.com>
2021-07-01 13:22:42 -0400

    Fixes bug where user was unable to quit

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,3 +1,6 @@
+#chatbot GUIDE From Professor Tony Hintons Class AI
+import pyttsx3
+
 #python program to subtract two numbers using function
 class Arithmetic(object):
     def __init__(self, a, b, operation):
@@ -8,9 +11,9 @@ class Arithmetic(object):
     def subtraction(self):  #function definifion for subtraction
         self.total = self.a - self.b
         self.operation_name = "Subtraction"
-        self.PrintArithmeticMessage()
+        self.PrintArthimeticMessage()
 
-    def PrintArithmeticMessage(self):
+    def PrintArthimeticMessage(self):
         print(f"\n\n{self.operation_name} is: {self.a} {self.operation} {self.b} = {self.total}\n\n")#call the function
     # #Class Project MathBot
     
@@ -20,7 +23,30 @@ class Arithmetic(object):
         self.total = self.a / self.b
         self.operation_name = "Division"
         # Call the PrintMessage method
-        self.PrintArithmeticMessage()
+        self.PrintArthimeticMessage()
+        
+    def read_text_outloud(text_to_read, print_message = False):# initialize the converter
+        converter = pyttsx3.init()
+        converter.setProperty('rate',130)
+        converter.setProperty('volume',0.7)
+
+        if print_message:
+            print(text_to_read)
+            converter.say(text_to_read)
+        else:
+            converter.say(text_to_read)
+
+        converter.runAndWait()
+        
+    def PromptUser():
+        d = dict()
+        dual_output = "Please enter the first number you want to use"
+        Arithmetic.read_text_outloud(dual_output)
+        d['a'] = int(input("Please enter the first number you want to use: "))
+        dual_output = "Please enter the second number you want to use"
+        Arithmetic.read_text_outloud(dual_output)
+        d['b'] = int(input("Please enter the second number you want to use: "))
+        return d
         
     # Declaring the method 
     def add(self):
@@ -28,7 +54,7 @@ class Arithmetic(object):
         self.total = self.a + self.b
         self.operation_name = "Addition"
         # Call the PrintMessage method
-        self.PrintArithmeticMessage()
+        self.PrintArthimeticMessage()
         
     #Declaring the Method 
     def multiplication(self):
@@ -36,25 +62,4 @@ class Arithmetic(object):
         self.total = self.a * self.b 
         self.operation_name = "Multiplication"
         # Call the PrintMessage method
-        self.PrintArithmeticMessage()
-    
-# #Class For Arithmetic.
-# #Being able to add two or more intergers together.
-# class Arithmetic:
-
-# #Letting the user put in input to Add two intergers
-# a=int(input("Enter First Number: ")) 
-# b=int(input("Enter Second Number: "))
-
-# #Creating an object for Addition
-# obj = Addition(a, b) 
-# #Prints the Answer to the user
-# print("Result: ",obj.add()) 
-
-# #Letting the user put in input to Multiply two intergers
-# a=int(input("Enter First Number: ")) 
-# b=int(input("Enter Second Number: "))
-# #Creating an object for Multiplication
-# obj =  Multiplication(a,b)
-# #Prints the Answer to the user
-# print("Result: ",obj.multiply()) 
+        self.PrintArthimeticMessage()
--- Tutor.py
+++ Tutor.py
@@ -1,19 +1,19 @@
 #CSC318 Team ChatBot
 
 # Heidi - Jeffrey - Deloris - & - Geri - UAT Summer 2021
-#-----------------------------------------------------------------Demo Example----------------------------------------------------------
-
+#-----------------------------------------------------------------Demo Example Sprint01----------------------------------------------------------
+#chatbot GUIDE From Professor Tony Hintons Class AI
 import pyttsx3
 # Class Project - sprint01 - Base Code Design - 06/17/2021
 # Here we put the import files
 from Arithmetic import Arithmetic
 #from Multiplication import Multiplication
 import random
-#function to read text to speech chatbot From Professor Tony Hintons Class AI @ UAT
+#function to read text to speech
 def read_text_outloud(text_to_read, print_message = False):
     # initialize the converter
     converter = pyttsx3.init()
-    converter.setProperty('rate',140)
+    converter.setProperty('rate',130)
     converter.setProperty('volume',0.7)
 
     if print_message:
@@ -23,37 +23,36 @@ def read_text_outloud(text_to_read, print_message = False):
         converter.say(text_to_read)
 
     converter.runAndWait()
-
-
+    
+dual_output = "The Mathematical Tutor Program"
+read_text_outloud(dual_output, True)
+dual_output ="Welcome To The Mathematical Tutor Program, We Are Going To Show You Some Math Equasions"
+read_text_outloud(dual_output, True)
+dual_output ="Choose From The Menu Below To Start Your Program And Follow The Instructions"
+read_text_outloud(dual_output, True)
 
 # Here we declare global variables
 program_over = True    # sets the main loop to run
-#End Of function to read text to speech chatbot From Professor Tony Hintons Class AI @ UAT
-print("\n\n\n\t\t\t\t\t\n The Mathematical Tutor Program")
 
-dual_output ="\t\t\n Welcome To The Mathematical Tutor Program"
-read_text_outloud(dual_output, True)
-dual_output ="\t\t\n Choose From The Menu Below To Start Your Program And Follow The Instructions"
-read_text_outloud(dual_output, True)
 # Here we will define our classes and functions
 class MainMenu:  # This is the main menu
 
     def intro():
-        dual_output ="\t\t\n In This Menu You Will Be Given A Number Of Options."
+        dual_output ="In This Menu You Will Be Given A Number Of Options."
         read_text_outloud(dual_output, True)
-        dual_output ="\t\t\n I Am Your Math Tutor Chatbot And I Am Here To Help You Learn."
+        dual_output ="I Am Your Math Tutor Chatbot And I Am Here To Help You Learn."
         read_text_outloud(dual_output, True)
-        dual_output ="\t\t\n Please Choose What You Would Like To Do Next: "
+        dual_output ="Please Choose What You Would Like To Do Next: "
         read_text_outloud(dual_output, True)
-        dual_output ="\t\t\n 1. Directions"
+        dual_output =" 1. Directions"
         read_text_outloud(dual_output, True)
-        dual_output ="\t\t\n 2. Start Program"
+        dual_output =" 2. Start Program"
         read_text_outloud(dual_output, True)
-        dual_output ="\t\t\n 3. Quit"
+        dual_output =" 3. Quit"
         read_text_outloud(dual_output, True)
-        dual_output = "\t\t\n What do you choose to do? "
+        dual_output = "What do you choose to do? "
         read_text_outloud(dual_output)
-        choice = input("\t\t\n\n What do you choose to do? ")
+        choice = input("\n What do you choose to do? ")
 
         if choice == "1":
             MainMenu.directions() # If user chooses 1 then go to MainMenu.directions()
@@ -65,28 +64,33 @@ class MainMenu:  # This is the main menu
             print("You Have Chosen To Quit, chat With You Next Time")
             program_over = False # If user chooses 3 then quit the program
             quit()
-            
         else:
-            dual_output = "\t\t\n Please try again."
+            dual_output = " Please try again."
             read_text_outloud(dual_output)
-            print("\t\t\n Please try again.") # If choice is not in the list, try again (exception handeling)
+            print(" Please try again.") # If choice is not in the list, try again (exception handeling)
             dual_output = "\n Please press Enter to continue."
             read_text_outloud(dual_output)
             press = input("\n Please press Enter to continue.")   
 
     def directions(): # The directions function
-        dual_output ="\t\t\n Greetings I am your mathematical tutor."
+        dual_output ="Greetings I am your mathematical tutor."
         print(dual_output)
         read_text_outloud(dual_output)
-        dual_output ="\t\t\n I am here to help you learn math."
+        dual_output ="I am here to help you learn math."
         print(dual_output)
         read_text_outloud(dual_output)
-        dual_output ="\t\t\n You can choose to do addition, subtraction, multiplication, or division."
+        dual_output ="You can choose to do addition, subtraction, multiplication, or division."
         print(dual_output)
         read_text_outloud(dual_output)
-        dual_output = "\t\t\n Please press Enter to continue."
+        dual_output ="If you solve the problem correctly you will earn score points."
+        print(dual_output)
         read_text_outloud(dual_output)
-        press = input("\t\t\n\n Please press Enter to continue.")
+        dual_output ="If you do not answer correctly, you will not get any score points."
+        print(dual_output)
+        read_text_outloud(dual_output)
+        dual_output = "Please press Enter to continue."
+        read_text_outloud(dual_output)
+        press = input("\n Please press Enter to continue.")
 
 class TheBrain:   # This class will be the main intelligence of the program.
    def divideInput(firstNumber, secondNumber):
@@ -103,53 +107,59 @@ class TheBrain:   # This class will be the main intelligence of the program.
 
    def start(): # This function is the main function that will give the user choices on what type of problems to solve
     
-    dual_output = "\t\t\n I have several options for you to learn from."
+    dual_output = "I have several options for you to learn from."
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n You may choose from the following tutorial programs."
+    dual_output = "You may choose from the following tutorial programs."
     read_text_outloud(dual_output, True)
     
-    dual_output = "\t\t\n 1. Addition" 
+    dual_output = "1. Addition" 
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n 2. Subtraction"
+    dual_output = "2. Subtraction"
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n 3. Multiplication"
+    dual_output = "3. Multiplication"
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n 4. Division"
+    dual_output = "4. Division"
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n 5 Quit"
+    dual_output = "5 Quit"
     read_text_outloud(dual_output, True)
-    dual_output = "\t\t\n What is your choice"
+    dual_output = "What is your choice"
     read_text_outloud(dual_output)
-    choice = input ("\t\t\n\n What is your choice: ") # this get input from the user
+    choice = input ("\nWhat is your choice: ") # this get input from the user
     
-    dual_output = "\t\t\n Please enter the first number you want to use"
-    read_text_outloud(dual_output)
-    firstNumber = int(input("\t\t\n Please enter the first number you want to use: "))
-    dual_output = "\t\t\n Please enter the second number you want to use"
-    read_text_outloud(dual_output)
-    secondNumber = int(input("\t\t\n Please enter the second number you want to use: "))
-
+    
     if choice == "1":
+        prompt_user = Arithmetic.PromptUser()
+        firstNumber = prompt_user['a']
+        secondNumber = prompt_user['a'] 
         TheBrain.addInput(firstNumber, secondNumber) # This is where the Addition Class will go
     elif choice == "2":
+        prompt_user = Arithmetic.PromptUser()
+        firstNumber = prompt_user['a']
+        secondNumber = prompt_user['a'] 
         TheBrain.subtractInput(firstNumber, secondNumber) # This is where the Subtraction Class will go
     elif choice == "3":
+        prompt_user = Arithmetic.PromptUser()
+        firstNumber = prompt_user['a']
+        secondNumber = prompt_user['a'] 
         TheBrain.multiplyInput(firstNumber, secondNumber) # This is where the Multiplication Class will go
     elif choice == "4":
+        prompt_user = Arithmetic.PromptUser()
+        firstNumber = prompt_user['a']
+        secondNumber = prompt_user['a'] 
         TheBrain.divideInput(firstNumber, secondNumber)
         
     elif choice == "5":
-        print("\t\t\n Thank you for playing! Goodbye.")
-        dual_output = "\t\t\n Thank you for playing! Goodbye."
+        print("Thank you for playing! Goodbye.")
+        dual_output = "Thank you for playing! Goodbye."
         read_text_outloud(dual_output)
         program_over = False # Quit the program
         quit()
     else:
-        print ("\t\t\n I am sorry can you please try again.")
-        dual_output = "\t\t\n I am sorry can you please try again."
+        print ("I am sorry can you please try again.")
+        dual_output = "I am sorry can you please try again."
         read_text_outloud(dual_output)
-        press = input("\t\t\n Please press Enter to continue")
-        dual_output = "\t\t\n Please press Enter to continue"
+        press = input("Please press Enter to continue")
+        dual_output = "Please press Enter to continue"
         read_text_outloud(dual_output)
         play = TheBrain.start()
 
@@ -157,4 +167,3 @@ class TheBrain:   # This class will be the main intelligence of the program.
 
 while program_over:
     game = MainMenu.intro()
-
